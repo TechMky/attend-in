e5c68bcf58fa7c9294a18f8ebe2cf6dd92ed71d1
Yashasvi Sinha <yashasvi.kumar.sinha@gmail.com>
2020-10-14 12:40:07 +0530

    Added Student List

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import students from './assets/student'
 import './App.css';
+import StudentList from './components/StudentList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StudentList students={students} />
     </div>
   );
 }
---
+++ src/assets/student.ts
@@ -0,0 +1,34 @@
+import Student from "../interface/student";
+const students: Student[] = [
+
+    {
+        name: 'ABHISHEK SINGH',
+    },
+    {
+        name: 'PALLAVI KUMARI GUPTA',
+    },
+    {
+        name: 'NEHA KUMARI SINGH',
+    },
+    {
+        name: 'RAJPREET KAUR',
+    },
+    {
+        name: 'GAURAV KHANNA',
+    },
+    {
+        name: 'SHRUTI SINGH',
+    },
+    {
+        name: 'SONAL SONKAR',
+    },
+    {
+        name: 'DIBYANSHI KUAMRI SHUKLA',
+    },
+    {
+        name: 'MEHWISH ANWAR',
+    },
+
+]
+
+export default students
